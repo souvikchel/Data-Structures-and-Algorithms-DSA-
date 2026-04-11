719ea6ab8c3a446516fc1613c124498eeb102a10
Souvik Chel <souvikchel@gmail.com>
2026-04-11 14:20:21 +0530

    updated

--- 31.Binary Tree/bt.cpp
+++ 31.Binary Tree/bt.cpp
@@ -237,9 +237,93 @@ void topView(Node* root){
   cout<<endl;
 }
 
+void KthHelper(Node* root,int k,int currLevel){//O(n)
+
+  if(root==NULL){
+    return;
+  }
+
+
+  if(currLevel==k){
+    cout<<root->data<<" ";
+    return;
+  }
+
+  KthHelper(root->left,k,currLevel+1);//left
+  KthHelper(root->right,k,currLevel+1);//right
+  cout<<endl;
+
+
+
+}
+
+void KthLevel(Node* root,int k){//O(n)
+  KthHelper(root,k,1);
+
+
+}
+
+bool rootToNodePath(Node* root,int n,vector<int> &path){//O(n)
+  if(root==NULL){
+    return false;
+  }
+  path.push_back(root->data);
+  if(root->data==n){
+    return true;
+  }
+  int isLeft=rootToNodePath(root->left,n,path);
+  int isRight=rootToNodePath(root->right,n,path);
+
+  if(isLeft || isRight){
+    return true;
+  }
+  path.pop_back();
+  return false;
+
+}
+
+int LCA(Node* root,int n1,int n2){//O(n) O(n)
+  vector<int> path1;
+  vector<int>path2;
+  rootToNodePath(root,n1,path1);
+  rootToNodePath(root,n2,path2);
+
+  int lca=-1;
+  for(int i=0,j=0;i<path1.size() && j<path2.size();i++,j++){
+    if (path1[i]!=path2[j]){
+      return lca;
+    }
+    lca=path1[i];
+  }
+  return lca;
+}
+
+Node* LCA2(Node* root ,int n1,int n2){
+  if(root==NULL){
+    return NULL;
+  }
+
+
+  if(root->data==n1 || root->data==n2){
+    return root;
+  }
+
+  Node* leftLCA=LCA2(root->left,n1,n2);
+  Node* rightLCA=LCA2(root->right,n1,n2);
+
+  if(leftLCA!=NULL && rightLCA!=NULL){
+    return root;
+  }
+
+  return leftLCA==NULL? rightLCA:leftLCA;
+}
+
 int main(){
   vector<int> nodes={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
   Node* root=buildTree(nodes);//1
+  //KthLevel(root,2);
+  int n1=4,n2=5;
+  cout << LCA2(root, n1, n2)->data<<endl;
 
  // cout<<"root= "<<root->data<<endl;
 
@@ -271,7 +355,7 @@ int main(){
  //subRoot->right=new Node(5);
 
  cout<<isSubtree(root,subRoot)<<endl;*/
-  topView(root);
+  //topView(root);
 
   return 0;
 }
